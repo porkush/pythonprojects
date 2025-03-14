a04995bf811d355030a78420107fbe6cfdcde1c8
porkush <parasmittal2018@gmail.com>
2025-03-15 02:05:58 +0530

    first project

---
+++ robobhasha.py
@@ -0,0 +1,11 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+
+print("Welcome to Robobhasha created by Paras")
+while True:
+    x = input("Enter what you want me to speak: ")
+    if x.lower() == "q":
+        engine.say("Bye bye friend")
+        break
+    engine.say(x)
